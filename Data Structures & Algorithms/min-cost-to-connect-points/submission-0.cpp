7665c10bd44a59a4a49a0aff22be8a2bd0d759f4
Nguyễn Duy Hưng <hungdevel2007@gmail.com>
2026-04-17 10:32:53 +0700

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+  int minCostConnectPoints(vector<vector<int>> &points) {
+    int n = points.size();
+    unordered_map<int, vector<pair<int, int>>> adj;
+    for (int i = 0; i < n; i++) {
+      int x1 = points[i][0];
+      int y1 = points[i][1];
+      for (int j = i + 1; j < n; j++) {
+        int x2 = points[j][0];
+        int y2 = points[j][1];
+        int dist = abs(x1 - x2) + abs(y1 - y2);
+        adj[i].push_back({dist, j});
+        adj[j].push_back({dist, i});
+      }
+    }
+    int res = 0;
+    unordered_set<int> visited;
+    priority_queue<pair<int, int>, vector<pair<int, int>>,
+                   greater<pair<int, int>>>
+        minH; // {cost, point}
+    minH.push({0, 0});
+    while (visited.size() < n) {
+      int cost = minH.top().first;
+      int i = minH.top().second;
+      minH.pop();
+      if (visited.find(i) != visited.end())
+        continue;
+      res += cost;
+      visited.insert(i);
+      for (const auto &nei : adj[i]) {
+        if (visited.find(nei.second) == visited.end()) {
+          // cost and i
+          minH.push({nei.first, nei.second});
+        }
+      }
+    }
+    return res;
+  }
+};
